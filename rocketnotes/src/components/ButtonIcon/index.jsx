9cda8a05d7e01741806c621c83471d740291224a
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-07-10 10:48:29 -0300

    Front-end removido

--- rocketnotes/src/components/ButtonIcon/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export function ButtonIcon({icon: Icon, size='24px', isActive = false, ...rest}){
-    return(
-        <Container type='button' $isactive={ isActive.toString() } { ...rest }>
-            {Icon && <Icon size={size} />}
-        </Container>
-    )
-}
--- rocketnotes/src/components/TextArea/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export function TextArea({...rest}){
-    return (
-        <Container>           
-            <textarea {...rest}></textarea>
-        </Container>           
-    )
-}
